f142c0188b641725f0d9556402e342b78ddd5e66
JiHwan Yeo <jihwan.yeo@samsung.com>
2020-05-26 17:11:01 +0900

    Applying NEMath to logistic kernel
    ONE-DCO-1.0-Signed-off-by: JiHwan Yeo <jihwan.yeo@samsung.com>

--- compute/cker/include/cker/operation/Logistic.h
+++ compute/cker/include/cker/operation/Logistic.h
@@ -20,6 +20,7 @@
 
 #include "cker/Shape.h"
 #include "cker/eigen/Utils.h"
+#include "cker/neon/NEMath.h"
 
 #include <cmath>
 #include <Eigen/Core>
@@ -32,9 +33,37 @@ namespace cker
 inline void Logistic(const Shape &input_shape, const float *input_data, const Shape &output_shape,
                      float *output_data)
 {
-  auto input_map = MapAsVector(input_data, input_shape);
-  auto output_map = MapAsVector(output_data, output_shape);
-  output_map.array() = input_map.array().unaryExpr(Eigen::internal::scalar_logistic_op<float>());
+  // auto input_map = MapAsVector(input_data, input_shape);
+  // auto output_map = MapAsVector(output_data, output_shape);
+  // output_map.array() = input_map.array().unaryExpr(Eigen::internal::scalar_logistic_op<float>());
+
+  UNUSED_RELEASE(output_shape);
+
+  // int total_input_size = input_shape.FlatSize();
+  size_t elements = input_shape.FlatSize();
+  const float *in = input_data;
+  float *out = output_data;
+  int nn = elements >> 2;
+  int remain = elements - (nn << 2);
+  float32x4_t _one = vdupq_n_f32(1.f);
+
+  for (; nn > 0; nn--)
+  {
+    float32x4_t _p = vld1q_f32(in);
+    _p = vnegq_f32(_p);
+    _p = arm_compute::vexpq_f32(_p);
+    _p = vaddq_f32(_p, _one);
+    float32x4_t _outp = arm_compute::vinvq_f32(_p);
+    vst1q_f32(out, _outp);
+    in += 4;
+    out += 4;
+  }
+  for (; remain > 0; remain--)
+  {
+    *out = 1.f / (1.f + std::exp(-*in));
+    in++;
+    out++;
+  }
 }
 
 } // namespace cker
